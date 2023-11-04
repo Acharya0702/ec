4045b44a3e8b6524bf9ccbe6ddfd1533ea15f4eb
GOURISANKAR ACHARYA <74904092+Acharya0702@users.noreply.github.com>
2023-11-04 17:22:30 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm, SetPasswordForm, PasswordResetForm
 from django.contrib.auth.models import User
 from . models import Customer
-from django.views.decorators.csrf import csrf_exempt
 
 class LoginForm(AuthenticationForm):
     username = UsernameField(widget=forms.TextInput(attrs={'autofocus':'true', 'class':'form-control'}))
